89158c363f15cd30365e886e88a5d6e2c5f1de78
Ilya Baykov <i-baykov@internet.ru>
2023-10-31 17:11:07 +0300

    Добавил функуию get_absolute_url, работающую с reverse

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ import django.db.models
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -37,3 +38,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])

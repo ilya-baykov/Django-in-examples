18472b5e49efa5c0d8684a0c26fd698bb3f42068
Ilya Baykov <i-baykov@internet.ru>
2023-11-02 11:40:42 +0300

    Добавил возможность оставлять комментарии на сайте ( под постами )

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 # Create your views here.
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
 
 
 def post_share(request, post_id):
@@ -45,9 +64,12 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post, 'comments': comments, 'form': form})
 
 
 class PostListView(ListView):

a20c8e802ec17da69426db86ae8fbf8b2c7733d4
Ilya Baykov <i-baykov@internet.ru>
2023-11-10 21:36:47 +0300

    Выгрузил все существующие данный из базы данный в формате JSON

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .sitemaps import PostSitemap
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -11,5 +11,7 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 
 ]
